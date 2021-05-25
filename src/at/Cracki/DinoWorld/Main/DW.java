9531c83186bf8adfa3901981f29e200d3452e946
BuildTools <unconfigured@null.spigotmc.org>
2021-05-25 18:44:51 +0200

    + Initial commit

---
+++ src/at/Cracki/DinoWorld/Main/DW.java
@@ -0,0 +1,18 @@
+package at.Cracki.DinoWorld.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DW extends JavaPlugin {
+
+    public static final String pre = "§6§lDino§e§lWorld";
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
